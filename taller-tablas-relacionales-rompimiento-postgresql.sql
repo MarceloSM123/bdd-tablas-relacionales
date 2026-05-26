41c9858c28ee108deac34369f49f3c469319bd60
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-05-25 21:15:19 -0500

    LMSM: Parte 2 – Crear Tablas creacion de las tablas libros y autores

--- taller-tablas-relacionales-rompimiento-postgresql.sql
+++ taller-tablas-relacionales-rompimiento-postgresql.sql
@@ -1,2 +1,17 @@
 -- PARTE 1 – CREAR BASE DE DATOS
 CREATE DATABASE biblioteca_db;
+
+-- PARTE 2 – CREAR TABLAS
+
+CREATE TABLE libros(
+    codigo VARCHAR(5) PRIMARY KEY,
+    titulo VARCHAR(50) NOT NULL,
+    pagina INT NOT NULL
+);
+
+CREATE TABLE autores(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    pais VARCHAR(30) NOT NULL
+);
+
