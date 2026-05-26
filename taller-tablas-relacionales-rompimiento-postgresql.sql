24c3cb1835c86cd9018f9ee4c8f0526e5438fd17
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-05-25 21:18:25 -0500

    LMSM: Parte 3 – Tabla de Rompimiento creacion de la tabla de rompimiento para libro-autor

--- taller-tablas-relacionales-rompimiento-postgresql.sql
+++ taller-tablas-relacionales-rompimiento-postgresql.sql
@@ -15,3 +15,14 @@ CREATE TABLE autores(
     pais VARCHAR(30) NOT NULL
 );
 
+-- PARTE 3 – TABLA DE ROMPIMIENTO 
+
+CREATE TABLE libro_autor(
+    la_libro_codigo_fk VARCHAR(5) NOT NULL,
+    la_autor_id_fk INT NOT NULL,
+    anio_publicacion INT NOT NULL,
+    CONSTRAINT libro_fk FOREIGN KEY(la_libro_codigo_fk) REFERENCES libros(codigo),
+    CONSTRAINT autor_fk FOREIGN KEY(la_autor_id_fk) REFERENCES autores(id),
+    CONSTRAINT libro_autor_pk PRIMARY KEY(la_libro_codigo_fk, la_autor_id_fk)
+);
+
